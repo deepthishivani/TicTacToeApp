5c4b0a9aaca360dabad93b5d4b10874b0e904b0b
Deepthi Shivani <deepthishivani@gmail.com>
2026-06-17 00:01:17 +0530

    UC2: Toss to decide first player and symbols

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,16 @@
+import java.util.Random;
+
 public class TicTacToe {
     static char[][] board = new char[3][3];
 
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
     public static void main(String[] args) {
         initializeBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
         printBoard();
     }
 
@@ -14,8 +22,36 @@ public class TicTacToe {
         }
     }
 
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            computerSymbol = 'X';
+            humanSymbol = 'O';
+        }
+    }
+
+    static void displayTossResult() {
+        System.out.println("Toss completed.");
+
+        if (isHumanTurn) {
+            System.out.println("Human starts first.");
+        } else {
+            System.out.println("Computer starts first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+
     static void printBoard() {
-        System.out.println("Tic-Tac-Toe Board:");
+        System.out.println("\nTic-Tac-Toe Board:");
 
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
